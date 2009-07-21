3ed9026d66e0a5a420b488d800d96e13322f2518
A. Klitzing <andre@incubo.de>
2009-07-21 17:29:03 +0200

    Add tests to found ALL (tagged) songs with disabled blacklist

--- tests/test_database.c
+++ tests/test_database.c
@@ -4,33 +4,75 @@
 #include "fixture_mpd.h"
 
 
+gint test_database_search_songs_blacklist(dbList** l_list)
+{
+	g_assert(l_list != NULL);
+	g_assert(*l_list == NULL);
+
+	gint count = 0;
+	MpdData* data;
+	for(data = mpd_database_get_complete(connection); data != NULL; data = mpd_data_get_next(data))
+	{
+		if(data->song->artist != NULL && data->song->artist != NULL)
+			*l_list = database_get_songs(*l_list, data->song->artist, data->song->title, &count);
+	}
+
+	/* check for untagged files */
+	const gchar* artist = "Ugly Kid Joe";
+	*l_list = database_get_songs(*l_list, artist, "12 Cents", &count);
+	*l_list = database_get_songs(*l_list, artist, "Oompa", &count); /* not blacklisted */
+
+	return count;
+}
+
 /* Check if every searched file in database is somehow blacklisted
    It must find nothing!
 */
-void test_database_search_songs_blacklist()
+void test_database_search_songs_blacklist_found_zero()
 {
 	fake_mpd_init(CONFIG_BL_ALL);
 
 	set_active_blacklist(TRUE);
 	dbList* list = NULL;
-	gint count = 0;
 
-	/* all blacklisted by album, genre or artist */
-	MpdData* data;
-	for(data = mpd_database_get_complete(connection); data != NULL; data = mpd_data_get_next(data))
-	{
-		if(data->song->artist != NULL && data->song->artist != NULL)
-			list = database_get_songs(list, data->song->artist, data->song->title, &count);
-	}
-
-	/* check for untagged files */
-	const gchar* artist = "Ugly Kid Joe";
-	list = database_get_songs(list, artist, "12 Cents", &count);
-	list = database_get_songs(list, artist, "Oompa", &count); /* not blacklisted */
+	/* all blacklisted by album, genre, artist or song */
+	gint count = test_database_search_songs_blacklist(&list);
 
 	g_test_message("count: %d", count);
 	g_assert(list == NULL);
 	g_assert(count == 0);
+
+	fake_mpd_free(CONFIG_BL_ALL);
+}
+
+void test_database_search_songs_blacklist_found_all()
+{
+	fake_mpd_init(CONFIG_BL_ALL);
+
+	set_active_blacklist(FALSE);
+	dbList* list = NULL;
+
+	/* nothing is blacklisted */
+	gint count = test_database_search_songs_blacklist(&list);
+	g_test_message("count: %d", count);
+	g_assert(list != NULL);
+	g_assert(count > 0);
+
+	/* check that EVERY song was found */
+	MpdData* data;
+	for(data = mpd_database_get_complete(connection); data != NULL; data = mpd_data_get_next(data))
+	{
+		if(data->song->artist != NULL && data->song->artist != NULL)
+			g_assert(exists_dbList(list, data->song->artist, data->song->title));
+	}
+
+	/* check for untagged files (must not found) */
+	const gchar* artist = "Ugly Kid Joe";
+	g_assert(!exists_dbList(list, artist, "12 Cents"));
+	g_assert(!exists_dbList(list, artist, "Oompa"));
+
+	/* 69 unique + 2 doubled (because of EXACT = FALSE)*/
+	g_assert(count == 71);
 
 	fake_mpd_free(CONFIG_BL_ALL);
 }
@@ -67,7 +109,8 @@ int main (int argc, char** argv)
 {
 	g_test_init(&argc, &argv, NULL);
 
-	g_test_add_func("/database/search/songs/blacklist", test_database_search_songs_blacklist);
+	g_test_add_func("/database/search/songs/blacklist/nofound", test_database_search_songs_blacklist_found_zero);
+	g_test_add_func("/database/search/songs/blacklist/found", test_database_search_songs_blacklist_found_all);
 	g_test_add_func("/database/search/artists/blacklist", test_database_search_artists_blacklist);
 
 	/* mute standard debug output from plugin */
